0260c8713def188aa699c7168680c199421bed03
matriplexim <matriplexim@users.noreply.github.com>
2023-01-17 15:59:25 +0400

    Initial Commit

---
+++ YouTubeClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YouTubeClone
+//
+//  Created by Максим Ломакин on 17.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
